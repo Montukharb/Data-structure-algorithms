cbfa84dc46001b6eb11cbc943bb6697a8108f773
montukharb <montukharb868@gmail.com>
2026-01-05 23:18:40 +0530

    updated

---
+++ DSA/Java/Patterns.java
@@ -0,0 +1,35 @@
+public class Patterns{
+    public static void main(String[] args)
+    {
+         System.out.println("hello");
+                   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void arr(){
+
+// int[][] arr= {{10,20,30,40,},{11,21,31,41},{41,32,43,54}};
+
+//          for(int i = 0; i<arr.length; i++)
+//          {
+//             for(int a = 0; a<arr[i].length; a++)
+//             {
+//                 System.out.print(arr[i][a] + " ");
+//             }
+//             System.out.println();
+//          }
+// }
+    
