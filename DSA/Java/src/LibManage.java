621932d7f0cb5cc93da2088d218a7ebd7cc94bf8
montukharb <montukharb868@gmail.com>
2026-01-15 18:22:19 +0530

    fix issue *

--- DSA/Java/src/LibManage.java
+++ DSA/Java/src/LibManage.java
@@ -1,19 +1,26 @@
 package mylibmange;
-public class LibManage{
- 
-   public void userData(){
-      String name = "John Don";
-      int age = 23;
-      System.out.println("Name = " + name);
-      System.out.println("age = " + age);
-      // gretting();
-   } 
-  public void gretting()
-   {
-      System.out.println("Today is Bad day");
-   }
+
+public class LibManage {
+
+    public void userData() {
+        String name = "John Don";
+        int age = 23;
+        System.out.println("Name = " + name);
+        System.out.println("age = " + age);
+        // gretting();
+    }
+    // public static void main(String[] args) {
+    //    LibManage obj = new LibManage();
+    //    obj.userData();
+    // } 
+
+    public void gretting() {
+        System.out.println("Today is Bad day");
+    }
 
 }
 
-
+//package createion command 
+// compile = javac -d . filename.java 
+// run  = java packagename.classname 
 
