cf69da2ee1748100f91690cc08aeb3fbd4c0b6e5
montukharb <montukharb868@gmail.com>
2026-01-30 22:40:49 +0530

    Level Order Traversal

--- DSA using Java/binaryTrees/BuildTreePreOrder.java
+++ DSA using Java/binaryTrees/BuildTreePreOrder.java
@@ -1,5 +1,8 @@
 package binaryTrees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // time complexity  = O(n);
 public class BuildTreePreOrder {
     static final int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -32,7 +35,7 @@ public class BuildTreePreOrder {
         return newNode; //return root node which is 1;
     }
 
-    void display(Node mainNode) {
+    void displayPreorder(Node mainNode) {
 
         if (mainNode == null) { //base condition root null or leaves null;
             System.out.print(-1 + " ");  //optional;
@@ -40,8 +43,58 @@ public class BuildTreePreOrder {
         }
 
         System.out.print(mainNode.data + " ");
-        display(mainNode.left);
-        display(mainNode.right);
+        displayPreorder(mainNode.left);
+        displayPreorder(mainNode.right);
+    }
+
+    void displayInorder(Node mainNode) {
+        if (mainNode == null) {
+            System.out.print(-1 + " ");
+            return;
+        }
+        displayInorder(mainNode.left);
+        System.out.print(mainNode.data + " ");
+        displayInorder(mainNode.right);
+    }
+
+    void displayPostorder(Node mainNode) {
+        if (mainNode == null) {
+            System.out.print(-1 + " ");
+            return;
+        }
+        displayPostorder(mainNode.left);
+        displayPostorder(mainNode.right);
+        System.out.print(mainNode.data + " ");
+    }
+
+    void levelOrderTraversal(Node mainNode) {
+        if (mainNode == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(mainNode); //root node;
+        queue.add(null); //second node null for next line;
+
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.remove();
+            if (currentNode == null) {
+                System.out.println();  //next line
+                if (queue.isEmpty()) {
+                    break;
+                } else {
+                    queue.add(null); //for next line;
+                }
+            } else {
+                System.out.print(currentNode.data + " ");
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+
+        }
     }
 
     static void main(String[] args) {
@@ -51,6 +104,13 @@ public class BuildTreePreOrder {
         Node rootNode = obj.buildTree(nodes);
         System.out.println("Root node of tree =  " + rootNode.data);
 
-        obj.display(rootNode);
+        System.out.println("\nPreorder Traversal");
+        obj.displayPreorder(rootNode);
+        System.out.println("\nInorder Traversal");
+        obj.displayInorder(rootNode);
+        System.out.println("\nPostorder Traversal");
+        obj.displayPostorder(rootNode);
+        System.out.println("\nLevel order Traversal using queue");
+        obj.levelOrderTraversal(rootNode);
     }
 }
