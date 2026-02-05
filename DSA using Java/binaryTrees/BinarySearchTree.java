47feb1fc605131b3c5d3538710782a47c4a7fa83
montukharb <montukharb868@gmail.com>
2026-02-05 22:41:12 +0530

    build bst tree, display inorder, preorder, search node, deleteNod, inorderSuccessor, printInRange, rootToLeafPath, valid bst, mirrorBst, balanceBst, bst to balanced bst conversion,

--- DSA using Java/binaryTrees/BinarySearchTree.java
+++ DSA using Java/binaryTrees/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package binaryTrees;
 
 
+import java.util.ArrayList;
+
 public class BinarySearchTree {
 
     static class Node {
@@ -40,6 +42,15 @@ public class BinarySearchTree {
         displayInorder(mainNode.right);
     }
 
+    void displayPreorder(Node mainNode) {
+        if (mainNode == null) {
+            return;
+        }
+        System.out.print(mainNode.data + " ");
+        displayInorder(mainNode.left);
+        displayInorder(mainNode.right);
+    }
+
     int searchNode(Node root, int key) {
         if (root == null) {
             return -1;
@@ -57,6 +68,9 @@ public class BinarySearchTree {
     }
 
     Node deleteNode(Node root, int data) {
+        if (root == null) {
+            return null;
+        }
         if (data < root.data) {
             root.left = deleteNode(root.left, data);
         } else if (data > root.data) {
@@ -93,8 +107,114 @@ public class BinarySearchTree {
         return root;
     }
 
+    void printInRange(Node root, int l_point, int r_point) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= l_point && root.data <= r_point) {
+
+            printInRange(root.left, l_point, r_point);
+            System.out.print(root.data + " ");
+            printInRange(root.right, l_point, r_point);
+
+        } else if (root.data > l_point) {
+            printInRange(root.left, l_point, r_point);
+
+        } else if (root.data < r_point) {
+            printInRange(root.right, l_point, r_point);
+
+        }
+    }
+
+    ArrayList<Integer> path = new ArrayList<>();
+
+    void rootToLeafPath(Node root) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            printPath();
+            System.out.println();
+        }
+        rootToLeafPath(root.left);
+        rootToLeafPath(root.right);
+        path.removeLast();
+
+    }
+
+    void printPath() {
+        for (var x : path) {
+            System.out.print(x + " ");
+        }
+    }
+
+    boolean isValidBst(Node root, Node min, Node max) {
+        if (root == null) {
+            return true; //empty tree already valid bst;
+        }
+        if (min != null && root.data <= min.data) {
+            return false;
+        } else if (max != null && root.data >= max.data) {
+            return false;
+        }
+        //left subtree call and right subtree call
+        return isValidBst(root.left, min, root) && isValidBst(root.right, root, max);
+    }
+
+    Node mirrorBst(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        mirrorBst(root.left);
+        mirrorBst(root.right);
+
+        return root;
+    }
+
+    ArrayList<Integer> sortedTree = new ArrayList<>();
+
+    Node balancedBst(int start, int end) {
+        int mid = start + (end - start) / 2;
+        if (start > end) {
+            return null;
+        }
+        Node root = new Node(sortedTree.get(mid));
+        root.left = balancedBst(start, mid - 1);
+        root.right = balancedBst(mid + 1, end);
+
+        return root;
+    }
+
+    ArrayList<Integer> inorderSequence = new ArrayList<>();
+
+    void inorderSeq(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderSeq(root.left);
+        inorderSequence.add(root.data);
+        inorderSeq(root.right);
+    }
+
+    Node bst_To_Balanced_Bst(int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        Node root = new Node(inorderSequence.get(mid));
+        root.left = bst_To_Balanced_Bst(start, mid - 1);
+        root.right = bst_To_Balanced_Bst(mid + 1, end);
+        return root;
+    }
+
     static void main(String[] args) {
-        int[] val = {6, 4, 5, 2, 3, 1, 8, 7, 9};
+//        int[] val = {6, 4, 5, 2, 3, 1, 8, 7, 9};
+        int[] val = {8, 5, 10, 3, 6, 11};
+
         Node root = null; //by default set null;
         BinarySearchTree obj = new BinarySearchTree();
         for (int j : val) {
@@ -113,5 +233,34 @@ public class BinarySearchTree {
         root = obj.deleteNode(root, 4);
 
         obj.displayInorder(root);
+        System.out.println("print in range 3 to 9");
+        obj.printInRange(root, 5, 9);
+
+        System.out.println("root to leaf all paths");
+        obj.rootToLeafPath(root);
+
+        if (obj.isValidBst(root, null, null)) {
+            System.out.println("valid bst");
+        } else {
+            System.out.println("not valid bst");
+        }
+
+        root = obj.mirrorBst(root);
+        obj.displayInorder(root);
+
+        int[] arr = {3, 5, 6, 8, 10, 11, 12};
+        for (var a : arr) {
+            obj.sortedTree.add(a);
+        }
+        root = obj.balancedBst(0, arr.length - 1);
+
+        System.out.println("\nbalanced binary tree");
+        obj.displayInorder(root);
+        obj.inorderSeq(root);
+
+        root = obj.bst_To_Balanced_Bst(0, obj.inorderSequence.size() - 1);
+
+        System.out.println("\nAfter converted bst to balanced bst");
+        obj.displayPreorder(root);
     }
 }
